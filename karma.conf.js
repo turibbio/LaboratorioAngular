709988f3d4667a8f86e096563b8ebd26e8e8ac32
turibbio <turibbio@live.com>
2019-09-15 16:00:16 +0200

    Updated karma.config

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_DEBUG,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     autoWatch: true,
     singleRun: true,
     restartOnFileChange: true
